9160bde92ea3b33f391a62f81e77130f4f081750
TasmiaH0508 <tasmiahaque2004@gmail.com>
2025-07-29 17:42:31 +0800

    Time: 0 ms (100%), Space: 41.4 MB (29.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -17,14 +17,18 @@ class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
-        } else if (p == null || q == null) {
+        } else if (p == null && q != null) {
             return false;
-        } else {
-            if (p.val != q.val) {
-                return false;
-            } else {
-                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-            }
+        } else if (p != null && q == null) {
+            return false;
         }
+
+        int pVal = p.val;
+        int qVal = q.val;
+        if (pVal != qVal) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
