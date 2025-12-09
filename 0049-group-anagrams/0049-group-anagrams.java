c26b12e15157fabb96f102f4851d09386c4165c9
TasmiaH0508 <tasmiahaque2004@gmail.com>
2025-12-09 22:37:04 +0800

    Time: 8 ms (40.62%), Space: 49.8 MB (27.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,34 +1,29 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        String[] stringsInSortedFormat = new String[strs.length];
-        for (int i = 0; i < strs.length; i++) {
-            stringsInSortedFormat[i] = copyStringAndSort(strs[i]);
-        }
-        Map<String, List<String>> anagrams = new HashMap<>();
-        for (int i = 0; i < strs.length; i++) {
-            if (anagrams.containsKey(stringsInSortedFormat[i])) {
-                List<String> ll = anagrams.get(stringsInSortedFormat[i]);
-                ll.add(strs[i]);
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            StringBuilder sb = new StringBuilder();
+            sb.append(arr);
+            String stringIndex = sb.toString();
+
+            if (map.containsKey(stringIndex)) {
+                List<String> ll = map.get(stringIndex);
+                ll.add(s);
+                map.put(stringIndex, ll);
             } else {
                 List<String> ll = new LinkedList<>();
-                ll.add(strs[i]);
-                anagrams.put(stringsInSortedFormat[i], ll);
+                ll.add(s);
+                map.put(stringIndex, ll);
             }
         }
+
         List<List<String>> res = new LinkedList<>();
-        for (String s : anagrams.keySet()) {
-            res.add(anagrams.get(s));
+        for (String key : map.keySet()) {
+            List<String> ll = map.get(key);
+            res.add(ll);
         }
         return res;
-    }
-
-    private String copyStringAndSort(String s) {
-        StringBuilder res = new StringBuilder();
-        char[] arr = s.toCharArray();
-        Arrays.sort(arr);
-        for (char c : arr) {
-            res.append(c);
-        }
-        return res.toString();
     }
 }
