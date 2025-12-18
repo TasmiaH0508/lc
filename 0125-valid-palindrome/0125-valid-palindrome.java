8bf7f86ae1059b11294a66608b1e16fead8b7f99
TasmiaH0508 <tasmiahaque2004@gmail.com>
2025-12-18 17:25:24 +0800

    Time: 3 ms (65.57%), Space: 45.7 MB (32.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String sInLowerCase = s.toLowerCase();
-        String sLetters = sInLowerCase.replaceAll("[^a-zA-Z0-9]", "");
-        char[] arr = sLetters.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        s = s.toLowerCase();
+        char[] arr = s.toCharArray();
+        for (char c : arr) {
+            int a = c;
+            if ((a >= 97 && a <= 122) || (a >= 48 && a <= 57)) {
+                sb.append(c);
+            }
+        }
+
+        String combined = sb.toString();
+        char[] cArr = combined.toCharArray();
         int i = 0;
-        int j = arr.length - 1;
-        while (i <= j) {
-            char leftChar = arr[i];
-            char rightChar = arr[j];
-            if (leftChar != rightChar) {
+        int j = cArr.length - 1;
+        while (i < j) {
+            if (cArr[i] != cArr[j]) {
                 return false;
             }
             i++;
