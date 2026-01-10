75796897d957d84a9de53e6ab2492baddb0d6e34
TasmiaH0508 <tasmiahaque2004@gmail.com>
2026-01-10 19:47:14 +0800

    Time: 14 ms (86.31%), Space: 93.4 MB (51.29%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> appeared = new HashSet<>();
+        for (int num : nums) {
+            if (appeared.contains(num)) {
+                return true;
+            }
+            appeared.add(num);
+        }
+        return false;
+    }
+}
