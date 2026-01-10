40093c0a5b65ce5cdfac11549fe3426ddaab7a72
TasmiaH0508 <tasmiahaque2004@gmail.com>
2026-01-10 19:44:59 +0800

    Time: 4 ms (51.29%), Space: 47 MB (14.58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
         s = s.toLowerCase();
         char[] arr = s.toCharArray();
+        Deque<Character> d = new ArrayDeque<>();
         for (char c : arr) {
-            int a = c;
-            if ((a >= 97 && a <= 122) || (a >= 48 && a <= 57)) {
-                sb.append(c);
+            if ((48 <= c && c <= 57) ||(97 <= c && c <= 122)) {
+                d.addLast(c);
             }
         }
 
-        String combined = sb.toString();
-        char[] cArr = combined.toCharArray();
-        int i = 0;
-        int j = cArr.length - 1;
-        while (i < j) {
-            if (cArr[i] != cArr[j]) {
+        while (d.size() > 0) {
+            char first = d.pollFirst();
+            char last = first;
+            if (d.size() > 0) {
+                last = d.pollLast();
+            }
+
+            if (first != last) {
                 return false;
             }
-            i++;
-            j--;
         }
         return true;
     }
