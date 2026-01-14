a111d0be16413545829485bd44fad26319de1b45
TasmiaH0508 <tasmiahaque2004@gmail.com>
2026-01-14 12:51:40 +0800

    Time: 0 ms (100%), Space: 44.6 MB (86.16%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums[nums.length - 1] < target) {
+            return nums.length;
+        }
+
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        if (nums[mid] < target) {
+            return mid + 1;
+        }
+        return mid;
+    }
+}
