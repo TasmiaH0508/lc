9cb980269636be4fafc4ae5377caaf95f1140ea9
TasmiaH0508 <tasmiahaque2004@gmail.com>
2026-03-28 13:49:16 +0800

    Time: 3 ms (58.6%), Space: 47.5 MB (7.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,31 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            List<Integer> ll = map.getOrDefault(nums[i], new LinkedList<>());
-            ll.add(i);
-            map.put(nums[i], ll);
-        }
-
+        Map<Integer, Integer> numToIndex = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int needed = target - nums[i];
-
-            if (map.containsKey(needed)) {
-                List<Integer> ll = map.get(needed);
-                int neededIndex = ll.get(0);
-
-                if (needed == nums[i]) {
-                    if (ll.size() >= 2) {
-                        neededIndex = ll.get(1);
-                    } else {
-                        continue;
-                    }
-                }
-
-                int[] res = new int[]{i, neededIndex};
-                return res;
+            if (numToIndex.containsKey(needed)) {
+                int index = numToIndex.get(needed);
+                return new int[]{index, i};
             }
+            numToIndex.put(nums[i], i);
         }
-        return null;
+        return new int[2];
     }
 }
