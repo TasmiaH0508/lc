97408cba9432eb203068b0d6a102aeab67a96ecd
TasmiaH0508 <tasmiahaque2004@gmail.com>
2026-03-30 18:27:27 +0800

    Time: 8 ms (39.46%), Space: 49.4 MB (89.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,21 +2,21 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
-            char[] sArr = s.toCharArray();
-            Arrays.sort(sArr);
-            StringBuilder sb = new StringBuilder();
-            sb.append(sArr);
+            char[] key = s.toCharArray();
+            Arrays.sort(key);
 
-            String key = sb.toString();
-            List<String> ll = map.getOrDefault(key, new LinkedList<>());
+            StringBuilder sbKey = new StringBuilder();
+            sbKey.append(key);
+            String keyString = sbKey.toString();
+            List<String> ll = map.getOrDefault(keyString, new LinkedList<>());
             ll.add(s);
-            map.put(key, ll);
+            map.put(keyString, ll);
         }
 
         List<List<String>> res = new LinkedList<>();
-        for (String key : map.keySet()) {
-            List<String> l = map.get(key);
-            res.add(l);
+        for (String sKey : map.keySet()) {
+            List<String> ll = map.get(sKey);
+            res.add(ll);
         }
         return res;
     }
