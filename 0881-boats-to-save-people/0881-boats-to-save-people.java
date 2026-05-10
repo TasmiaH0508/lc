ae84db472d67cd463853ef1e73bafeb3c60f4fd2
TasmiaH0508 <tasmiahaque2004@gmail.com>
2026-05-10 18:30:32 +0800

    Time: 20 ms (83.65%), Space: 56.7 MB (21.11%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,20 +1,25 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
+
         int low = 0;
         int high = people.length - 1;
-
         int count = 0;
         while (low <= high) {
-            int lowVal = people[low];
-            int highVal = people[high];
+            if (low == high) {
+                count++;
+                break;
+            }
 
-            count++;
-            if (lowVal + highVal <= limit) {
-                low++;
+            int totalWeight = people[low] + people[high];
+
+            if (totalWeight <= limit) {
+                count++;
                 high--;
+                low++;
             } else {
                 high--;
+                count++;
             }
         }
         return count;
