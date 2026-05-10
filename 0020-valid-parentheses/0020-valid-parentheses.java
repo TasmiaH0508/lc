7bd53e2298ad7a646471d5c33a8d38a0abc8fb3e
TasmiaH0508 <tasmiahaque2004@gmail.com>
2026-05-10 14:21:53 +0800

    Time: 3 ms (87.69%), Space: 43.3 MB (57.1%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-        char[] arr = s.toCharArray();
         Stack<Character> stack = new Stack<>();
-        for (char c : arr) {
-            boolean isOpening = c == '(' || c == '{' || c == '[';
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            boolean isOpening = arr[i] == '(' || arr[i] == '[' || arr[i] == '{';
             if (isOpening) {
-                stack.push(c);
-                continue;
-            }
-
-            char prev = '0';
-            if (!stack.empty()) {
-                prev = stack.peek();
-            }
-
-            boolean isMatch = (prev == '(' && c == ')') || 
-                                (prev == '[' && c == ']') ||
-                                (prev == '{' && c == '}');
-            if (isMatch) {
-                stack.pop();
+                stack.push(arr[i]);
             } else {
-                return false;
+                char prev = '0';
+                if (!stack.empty()) {
+                    prev = stack.peek();
+                }
+
+                boolean isMatch = (prev == '(' && arr[i] == ')') || 
+                                    (prev == '[' && arr[i] == ']') ||
+                                    (prev == '{' && arr[i] == '}');
+                if (isMatch) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
             }
         }
-        return stack.empty();
+        return stack.size() == 0;
     }
 }
